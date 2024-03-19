9afe28ccd542a7d3fd09d3d33da5b88b98aac2f2
Stijn <stijn@forcai.eu>
2024-03-19 11:27:14 +0100

    zo

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ tabs = st.tabs(['Tempo → km/h', 'Km/h → tempo', 'Target → speed'])
 
 
 with tabs[0]:
-    st.subheader('Tempo to km/h')
+    st.subheader('Tempo → km/h')
     with st.container(border=True):
         minutes = st.number_input('minutes',min_value=0, max_value=60, step=1, value=6)
         seconds = st.number_input('seconds',min_value=0, max_value=60, step=1)
@@ -16,7 +16,7 @@ with tabs[0]:
         
 
 with tabs[1]:
-    st.subheader('Km/h to tempo')
+    st.subheader('Km/h → tempo')
     with st.container(border=True):
         kmh = st.number_input('km/h',min_value=0.0, value=10.0,max_value=60.0, step=0.1)
         
@@ -29,7 +29,7 @@ with tabs[1]:
     
     
 with tabs[2]:
-    st.subheader('Target to speed')
+    st.subheader('Target → speed')
     with st.container(border=True):
         km = st.number_input('km',min_value=0.0, step=0.01, value=5.0)
         target_minutes = st.number_input('minutes',key=2,min_value=0, step=1, value=30)
